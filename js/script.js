e091bbdcc8519872d4dd61679b633e740d7d50d0
laivii <91199307+laivii@users.noreply.github.com>
2022-08-22 19:44:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+$(".bingo-card__item").on('click', function() {
+        $(this).toggleClass('active');  
+});
+
+$('.clear-button').on('click', function(){
+        $('.bingo-card__item').removeClass('active');
+});
+
+
